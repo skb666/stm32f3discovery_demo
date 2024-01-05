7497a8a5311432e4b372d20d9aab18564370183f
skb666 <skb666@qq.com>
2024-01-05 14:31:50 +0800

    更新 `RING_FIFO`

--- bld/User/RING_FIFO/src/ring_fifo.c
+++ bld/User/RING_FIFO/src/ring_fifo.c
@@ -136,6 +136,27 @@ void *ring_peek(RING_FIFO *ring) {
   return (uint8_t *)ring->buffer + ring->head * ring->element_size;
 }
 
+void *ring_peek_next(RING_FIFO *ring, void *ptr) {
+  NUM_TYPE index = 0;
+
+  if (ring == NULL || ring->size == 0) {
+    return NULL;
+  }
+
+  if (ptr == NULL) {
+    // 返回头部指针
+    return (uint8_t *)ring->buffer + ring->head * ring->element_size;
+  }
+
+  index = ((uint8_t *)ptr - (uint8_t *)ring->buffer) / ring->element_size + 1;
+  index %= ring->capacity;
+  if (index != ring->tail) {
+    return (uint8_t *)ring->buffer + index * ring->element_size;
+  }
+
+  return NULL;
+}
+
 NUM_TYPE ring_push_mult(RING_FIFO *ring, const void *elements, NUM_TYPE num) {
   uint8_t *inbuf = NULL;
   uint8_t *outbuf = NULL;
@@ -261,7 +282,7 @@ int8_t ring_deep_copy(RING_FIFO *ring_dst, RING_FIFO *ring_src) {
   if ((ring_dst->capacity != ring_src->capacity) || (ring_dst->element_size != ring_src->element_size)) {
     return -1;
   }
-  
+
   memcpy(ring_dst->buffer, ring_src->buffer, ring_src->capacity * ring_src->element_size);
   ring_dst->cover = ring_src->cover;
   ring_dst->head = ring_src->head;
