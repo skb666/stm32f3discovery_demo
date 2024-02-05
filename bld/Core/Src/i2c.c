c7db5226c938c203b64e83fd64cf72cf3b008fae
skb666 <973689813@qq.com>
2024-02-05 23:54:41 +0800

    降低 I2C 速率

--- bld/Core/Src/i2c.c
+++ bld/Core/Src/i2c.c
@@ -69,7 +69,7 @@ void MX_I2C2_Init(void)
   LL_I2C_DisableGeneralCall(I2C2);
   LL_I2C_EnableClockStretching(I2C2);
   I2C_InitStruct.PeripheralMode = LL_I2C_MODE_I2C;
-  I2C_InitStruct.Timing = 0x0000020B;
+  I2C_InitStruct.Timing = 0x2000090E;
   I2C_InitStruct.AnalogFilter = LL_I2C_ANALOGFILTER_ENABLE;
   I2C_InitStruct.DigitalFilter = 0;
   I2C_InitStruct.OwnAddress1 = 0;
